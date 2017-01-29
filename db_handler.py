ac55bf4c4394a2394a1d26e2b6274d68d3e5a2ab
Noam Raz <noamraz8@gmail.com>
2017-01-29 19:31:50 +0200

    Implement database lookup

---
+++ db_handler.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import sqlite3 as sql
+
+class DBHandler(object):
+    '''Holds a database connection and allows for descriptions to be found for corresponding
+    syscall names
+    '''
+    def __init__(self, path):
+        '''Initializes the connection with the database specified by `path`'''
+        self.con = sql.connect(path)
+
+    def __enter__(self):
+        pass  # nothing to do here...
+
+    def __exit__(self, exc_type, exc, trace):
+        '''Cleans up resources (the database connection)'''
+        self.con.close()
+
+    def lookup(self, name):
+        '''Looks up the specified syscall in the database and returns its description, if any'''
+        cur = self.con.cursor()
+        cur.execute("SELECT description FROM tab WHERE name='{}'".format(name))
+        row = cur.fetchone()  # there shouldn't be more than one entry anyway
+        if row:
+            return row[0]
