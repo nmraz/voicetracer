2dc89e346316548ecd0602cf55efeb2e2039292e
Noam Raz <noamraz8@gmail.com>
2017-01-29 23:07:43 +0200

    Implement basic parsing/formatting of trace file

---
+++ trace_parsing.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+def fmt_trace_line(line, db):
+    '''Parses the line from the trace, and formats it according to the appropriate entry in `db`'''
+    # TODO: do a real parse here, not just a bunch of splits
+    # (simple splitting doesn't play nicely with string literals)
+    fn_start = line.find('(')
+    if fn_start == -1:
+        return  # some other message (not a function call)
+    name = line[:fn_start]
+    fmt = db.lookup(name)
+    if not fmt:
+        return
+    rest = line[fn_start + 1:]
+    fn_end = rest.find(')')
+    args = map(lambda x: x.strip(), rest[:fn_end].split(','))
+    eq_pos = rest[fn_end:].find('=')
+    ret = rest[fn_end + eq_pos + 1:].strip()
+    return fmt.format(*(args + [ret]))
