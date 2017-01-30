c45949790a2735a3432f41c9c362fcc977f5d967
Noam Raz <noamraz8@gmail.com>
2017-01-30 22:50:17 +0200

    Fix watcher behavior
    Previously, the watcher did not watch the file at all, but
    rather simply read its current state and exited. The watcher now
    busy waits for new lines, and only quits when explicitly told to
    (via __exit__).

--- watcher.py
+++ watcher.py
@@ -2,6 +2,7 @@
 
 import os
 import threading
+import time
 from db_handler import DBHandler
 
 class Watcher(object):
@@ -9,19 +10,32 @@ class Watcher(object):
     def __init__(self, db_path, trace_path):
         '''Initializes with the given trace file and database'''
         self.db = DBHandler(db_path)
-        self.trace = open(trace_path)
+        self.trace = open(trace_path, 'r')
         self.thr = threading.Thread(target = lambda: self.run())
+
+        self.should_quit = False
+        self.quit_lock = threading.Lock()  # protects `should_quit`
 
     def __enter__(self):
         '''Starts the watcher thread'''
         self.thr.start()
 
     def __exit__(self, exc_type, exc, trace):
+        '''Requests the watcher to quit and frees its resources'''
+        with self.quit_lock:
+            self.should_quit = True
         self.thr.join()  # wait first to prevent concurrent access to resources
         self.trace.close()
         self.db.__exit__(exc_type, exc, trace)
 
     def run(self):
         '''The actual thread callback'''
-        for line in self.trace:
-            pass  # TODO: implement trace parsing/speaking
+        while True:
+            new_line = self.trace.readline()
+            if new_line:
+                print new_line  # TODO: handle new message here
+            else:
+                with self.quit_lock:
+                    if self.should_quit:
+                        return
+                time.sleep(0.0001)  # yield execution to another thread
