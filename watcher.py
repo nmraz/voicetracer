24a44cc3fd58b615c6b6a637f729333e5ab02863
Noam Raz <noamraz8@gmail.com>
2017-01-30 23:20:34 +0200

    Move resource acquisition to watcher thread
    This is necessary as a database connection may only be accessed on the
    thread on which it was created.

--- watcher.py
+++ watcher.py
@@ -9,33 +9,34 @@ class Watcher(object):
     '''Watches a trace file and speaks whenever a known systcall is encountered'''
     def __init__(self, db_path, trace_path):
         '''Initializes with the given trace file and database'''
-        self.db = DBHandler(db_path)
-        self.trace = open(trace_path, 'r')
+        self.db_path = db_path
+        self.trace_path = trace_path
         self.thr = threading.Thread(target = lambda: self.run())
 
         self.should_quit = False
         self.quit_lock = threading.Lock()  # protects `should_quit`
 
     def __enter__(self):
-        '''Starts the watcher thread'''
+        '''Starts the watcher thread and creates resources'''
         self.thr.start()
 
     def __exit__(self, exc_type, exc, trace):
         '''Requests the watcher to quit and frees its resources'''
         with self.quit_lock:
             self.should_quit = True
-        self.thr.join()  # wait first to prevent concurrent access to resources
-        self.trace.close()
-        self.db.__exit__(exc_type, exc, trace)
+        self.thr.join()
 
     def run(self):
         '''The actual thread callback'''
-        while True:
-            new_line = self.trace.readline()
-            if new_line:
-                print new_line  # TODO: handle new message here
-            else:
-                with self.quit_lock:
-                    if self.should_quit:
-                        return
-                time.sleep(0.0001)  # yield execution to another thread
+        db = DBHandler(self.db_path)
+        trace = open(self.trace_path, 'r')
+        with trace, db:
+            while True:
+                new_line = trace.readline()
+                if new_line:
+                    print new_line  # TODO: handle new message here
+                else:
+                    with self.quit_lock:
+                        if self.should_quit:
+                            return
+                    time.sleep(0.0001)  # yield execution to another thread
