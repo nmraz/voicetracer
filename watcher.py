05574debb8cde1fdf16cf6c33ed20775735a9dc3
Noam Raz <noamraz8@gmail.com>
2017-01-31 17:43:59 +0200

    Handle ctrl+c correctly in watcher

--- watcher.py
+++ watcher.py
@@ -2,6 +2,7 @@
 
 from db_handler import DBHandler
 from trace_parsing import fmt_trace_line
+import os
 import subprocess
 import threading
 import time
@@ -14,7 +15,8 @@ class Watcher(object):
         self.trace_path = trace_path
         self.thr = threading.Thread(target = lambda: self.run())
 
-        self.should_quit = False
+        # 0 = don't quit, 1 = quit when done, 2 = quit now
+        self.should_quit = 0
         self.quit_lock = threading.Lock()  # protects `should_quit`
 
     def __enter__(self):
@@ -24,8 +26,13 @@ class Watcher(object):
     def __exit__(self, exc_type, exc, trace):
         '''Requests the watcher to quit and frees its resources'''
         with self.quit_lock:
-            self.should_quit = True
-        self.thr.join()
+            self.should_quit = 2 if exc_type else 1
+        try:
+            while self.thr.is_alive():
+                self.thr.join(0.0001)  # workaround to allow KeyboardInterrupt during join
+        except:
+            with self.quit_lock:
+                self.should_quit = 2
 
     def run(self):
         '''The actual thread callback'''
@@ -33,13 +40,18 @@ class Watcher(object):
         trace = open(self.trace_path, 'r')
         with trace, db:
             while True:
+                with self.quit_lock:
+                    should_quit = self.should_quit
+                if should_quit == 2:  # quit now
+                    return
+
                 new_line = trace.readline()
                 if new_line:
                     msg = fmt_trace_line(new_line, db)
                     if msg:
-                        subprocess.call(['espeak', '{}'.format(msg)])
+                        subprocess.call(['espeak', '{}'.format(msg)],
+                            preexec_fn = lambda: os.setpgrp())  # detach from main process group
                 else:
-                    with self.quit_lock:
-                        if self.should_quit:
-                            return
+                    if should_quit:
+                        return
                     time.sleep(0.0001)  # yield execution to another thread
