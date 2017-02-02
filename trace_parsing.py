9473c976d0127a7deedc6ad0ec53d21d0a6fe82c
Noam Raz <noamraz8@gmail.com>
2017-02-02 14:05:53 +0200

    Implement correct parsing of strace messages
    Previously, the "parser" simply split on commas, which typically
    resulted in incorrect interpretations of the string. The new parser
    handles things (mostly) correctly, while serendipitously giving the
    format string much finer-grained control over which parts of the string
    it wants.

--- trace_parsing.py
+++ trace_parsing.py
@@ -1,34 +1,204 @@
 #!/usr/bin/python
 
-def succeeded(ret_err):
+import re
+
+class Stream(object):
+    '''Represents a character stream and a token position inside it'''
+    def __init__(self, str, pos = 0, start = 0):
+        '''Initializes the stream with `str` and sets its token start and end to 0'''
+        self.str = str
+        self.pos = pos
+        self.start = start
+
+    def peek(self):
+        '''Returns the next character'''
+        if not self.eof():
+            return self.str[self.pos]
+
+    def next(self):
+        '''Advances the stream and returns the next character, if possible'''
+        next = self.peek()
+        self.pos += 1
+        return next
+
+    def eat(self, chs):
+        '''If the next character is one of `chs`, advances the stream and returns True; returns
+        False otherwise
+        '''
+        if re.search('[{}]'.format(chs), self.peek() or ''):
+            self.next()
+            return True
+        return False
+
+    def eat_while(self, chs):
+        '''Advances the stream until a character which isn't one of `chs` is encountered'''
+        while self.eat(chs):
+            pass
+
+    def eat_tok(self, ch):
+        '''Attempts to consume `ch` as a unique token'''
+        self.newtok()
+        consumed = self.eat(ch)
+        self.newtok()
+        return consumed
+
+    def eof(self):
+        '''Returns whether the token pos is at the end of the stream'''
+        return self.pos >= len(self.str)
+
+    def newtok(self):
+        '''Skips any whitespace and sets the token start to the current position, starting a
+        new token
+        '''
+        self.eat_while('\s')
+        self.start = self.pos
+
+    def curtok(self):
+        '''Returns the current token'''
+        return self.str[self.start:self.pos]
+
+
+class Val(object):
+    '''An object which may be used as either a string, a dictionary, or a list.
+    These qualities make it easy to format the object into format strings expecting objects with
+    various characteristics (e.g., expects a key `sa_family`, which may or may not exist)
+    '''
+    def __init__(self, val = None):
+        '''Initializes with the given value and type'''
+        self.val = val
+
+    def __getitem__(self, key):
+        '''Behave like a list or a dictionary'''
+        if ((type(self.val) is list and 0 <= key < len(self.val))
+            or (type(self.val) is dict and key in self.val)):
+            return self.val[key]
+        return Val()  # allow for recursive operations
+
+    def __str__(self):
+        '''Behave like a string (simply return string representation of underlying object)'''
+        if type(self.val) is dict:
+            # recursively stringify dictionary
+            return str({key: str(val) for key, val in self.val.iteritems()})
+        elif type(self.val) is list:
+            # recursively stringify list
+            return str([str(item) for item in self.val])
+        else:
+            return str(self.val)
+
+# The following functions implement a recursive-descent parser for the c-like syntax which strace
+# uses, with one caveat: expressions are not parsed fully. This may, on rare occasions, lead to
+# misinterpretation of the string, although it is much better than having no parser at all.
+
+def eat_string(stream):
+    '''Advances the stream to the end of the string, respecting escape sequences'''
+    escaped = False  # are we in an escape sequence?
+    ch = stream.next()
+    while ch is not None:
+        if ch == '"' and not escaped: break  # end of string
+        escaped = ch == '\\' and not escaped  # every backslash toggles "escapiness"
+        ch = stream.next()
+
+def parse_array(stream, term):
+    '''Parses an array terminated by `term`, and returns a list of the elements'''
+    # for example, to parse '[a, b, c]',
+    # advance past the first '[' and call parse_array(stream, ']')
+    # note: parse_array will see this as 'a, b, c]'
+    ret = []
+    if not stream.eat_tok(term):  # non-emptiness check
+        ret.append(parse_expr(stream, ',' + term))  # use ',' and term as sync tokens
+        while stream.eat_tok(','):  # comma-separated
+            ret.append(parse_expr(stream, ',' + term))
+    stream.eat_tok(term)
+    return ret
+
+def parse_struct_field(stream):
+    '''Parses something of the form 'name=expr' and returns a tuple (name, expr) or None on failure'''
+    stream.newtok()
+    stream.eat_while('\w')  # identifier
+    name = stream.curtok()
+    if not stream.eat_tok('='):
+        return None
+    expr = parse_expr(stream, ',}')  # use ',}' as sync tokens
+    return (name, expr)
+
+def parse_struct(stream):
+    '''Parses a c-struct-like construct and returns an dictionary Val'''
+    # e.g. '{a=1, b=2}' => Val({'a': 1, 'b': 2})
+    ret = {}
+    if not stream.eat_tok('}'):  # non-emptiness check
+        name, expr = parse_struct_field(stream)  # this is definitely safe, see parse_expr
+        ret[name] = expr
+        while stream.eat_tok(','):
+            data = parse_struct_field(stream)
+            if not data:  # an unnamed field within a struct?
+                stream.eat_while('^,}')  # jump to nearest sync token
+                continue
+            ret[data[0]] = data[1]
+    stream.eat_tok('}')
+    return Val(ret)
+
+def parse_expr(stream, terms):
+    '''Attempts to parse an expression, and jumps to one of `terms` at the end/at failure.
+    Returns a Val representing what it parsed (e.g. a struct, array, string)
+    '''
+    ret = Val()
+    stream.newtok()
+    if stream.eat('"'):  # start of string
+        eat_string(stream)
+        ret = Val(stream.curtok())
+    elif stream.eat_tok('['):  # start of array
+        ret = Val(parse_array(stream, '\]'))
+    elif stream.eat_tok('{'):  # either struct OR array
+        tmp_stream = Stream(stream.str, stream.pos, stream.start)
+        if parse_struct_field(tmp_stream):
+            ret = parse_struct(stream)
+        else:
+            ret = Val(parse_array(stream, '}'))  # this can happen in select, for example
+    else:
+        stream.eat_while('^(' + terms)  # not a very good parse
+        name = stream.curtok().strip()
+        if stream.eat_tok('('):
+            # function call: return [name, arg1, arg2, ...]
+            ret = Val([name] + parse_array(stream, ')'))
+        else:
+            # give up...
+            ret = Val(name)
+    stream.eat_while('^' + terms)
+    return ret
+
+def parse_sys_call(stream):
+    '''Parses to the end of the system call, and returns (args, ret, err), where args is a list of
+    the arguments, ret is the return value, and err is the description following it
+    '''
+    args = parse_array(stream, ')')
+    if not stream.eat_tok('='):
+        return (args, None, None)
+    stream.eat_while('\S')
+    ret = stream.curtok()
+    stream.newtok()
+    stream.pos = len(stream.str)  # just take everything at this point
+    err = stream.curtok()
+    return (args, ret, err)
+
+
+def succeeded(ret, err):
     '''Returns a string describing the function's success/failure,
     based on its return value and errno
     '''
-    ret_err = ret_err.split(' ', 1)
-    try:
-        ret = int(ret_err[0])
-    except ValueError:
-        return 'succeded'  # just assume success if the return value isn't integral
-    if ret != -1:
-        return 'succeded'
-    return ('failed with error {}'.format(ret_err[1]) if len(ret_err) == 2
-        else 'failed with unknon error')
+    if ret == '-1':
+        return 'failed with error {}'.format(err) if err else 'failed'
+    else:
+        return 'succeeded with {}'.format(err) if err else 'succeeded'
 
 def fmt_trace_line(line, db):
     '''Parses the line from the trace, and formats it according to the appropriate entry in `db`'''
-    # NOTE: this is horrendously broken and should be replaced with a real parse!!!
-    fn_start = line.find('(')
-    if fn_start == -1:
-        return  # some other message (not a function call)
-    name = line[:fn_start]
-    fmt = db.lookup(name)
-    if not fmt:
+    stream = Stream(line)
+    stream.eat_while('\w')
+    name = stream.curtok()
+    if not stream.eat_tok('('):  # not a function call
         return
-    rest = line[fn_start + 1:]
-    fn_end = rest.find(')')  # end of the call
-    args = map(lambda x: x.strip(),
-        rest[:fn_end].split(','))
-    eq_pos = rest[fn_end:].find('=')
-    ret = rest[fn_end + eq_pos + 1:].strip()  # return value is after the `=` sign
-    # format [arg1, arg2, ..., argN, ret], {succeeded} into the string
-    return fmt.format(*(args + [ret]), succeeded = succeeded(ret))
+    fmt = db.lookup(name)
+    if not fmt:  # not in the database
+        return
+    args, ret, err = parse_sys_call(stream)
+    return fmt.format(*args, ret = ret, succeeded = succeeded(ret, err))
